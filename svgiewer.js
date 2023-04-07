9d99c50a54e92bc0b4b04fd20eba7ecc210c089e
dann <gh@mrmuster.com>
2023-04-07 18:18:47 -0400

    Now with more gas

--- svgiewer.js
+++ svgiewer.js
@@ -195,7 +195,7 @@ function stack_lines(env) {                  // one-pass line aligner, B- for sp
 
 
 function plonk_twists(env) {
-    let x = 0, gas = 10000, mind = 20
+    let x = 0, gas = 1000000, mind = 20      // gas gets us unstuck if this all goes wrong
     let lines = env.firsts.slice().reverse()
     while(lines.length) {                    // rules: teth + posts + hoisting all required before plonking
         lines = lines.map(t => {
